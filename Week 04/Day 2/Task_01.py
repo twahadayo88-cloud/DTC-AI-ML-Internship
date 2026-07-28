e4de3e9b02a4d04a43836e6fbd4cbf6f99155f79
Muhammad Twaha <twahadayo88@gmail.com>
2026-07-28 12:19:03 +0500

    Implement missing value handling, label encoding and one hot encoding

--- Week 04/Day 2/Task_01.py
+++ Week 04/Day 2/Task_01.py
@@ -75,8 +75,9 @@ print(df)"""
 
 #____________________________________________________________________________________________
 # Encoding
+#Label encodeing
 
-import pandas as pd
+""""import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("Week 04/Day 2/Students.csv")
@@ -108,6 +109,51 @@ encoder = LabelEncoder()
 df["Gender"] = encoder.fit_transform(df["Gender"])
 
 print(df)
+
+print("\nMapping")
+print(encoder.classes_)"""
+
+#----------------------------------------------------------------------------
+# Encoding
+# One Hot Encoding
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv("Week 04/Day 2/Students.csv")
+
+print("Orignal shape")
+print(df.shape)
+
+print(df["Age"].mean())
+print(df["Marks"].mean())
+
+df["Age"] = df["Age"].fillna(df["Age"].mean())
+df["Marks"] = df["Marks"].fillna(df["Marks"].mean())
+
+
+
+#print(df["City"].mode())
+print(df["City"].mode())
+
+#df["City"] = df["City"].fillna(df["City"].mean())
+df["City"] = df["City"].fillna(df["City"].mode()[0])
+
+# Remove name
+df = df.drop("Name",axis=1)
+
+#Create encoder
+encoder = LabelEncoder()
+
+#Encode gender
+df["Gender"] = encoder.fit_transform(df["Gender"])
+df = pd.get_dummies(df,columns=["City"])
+print("\nAfter one hot encoding")
+
+#print(df)
+pd.set_option("display.max_columns",None)
+print(df)
+print(df.columns)
 
 print("\nMapping")
 print(encoder.classes_)
