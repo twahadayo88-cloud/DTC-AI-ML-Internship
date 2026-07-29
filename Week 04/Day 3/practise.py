4c5ac5aebd61e941978c2bef4a058560f85ddcf5
Muhammad Twaha <twahadayo88@gmail.com>
2026-07-29 11:25:16 +0500

    Week 04 day 3: Added data inspection, missing value analysis, and feature engineering using pandas

---
+++ Week 04/Day 3/practise.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+df = pd.read_csv("Week 04/Day 3/students_data.csv")
+
+print(df)
+print(df.info())
+print(df.shape)
+print(df.columns)
+print(df.isnull().sum())
+print(df.duplicated().sum())
+print(df.describe())
+print(df.dtypes)
+
+#--------------------------------------------------
+# Feature Engineering
+#creating age groups
+
+df["Age_Group"] = pd.cut(
+    df["Age"],
+    bins=[0,20,23,100],
+    labels=["Young","Adult","Senior"]
+
+)
+
+print(df[["Age","Age_Group"]])
+
+#Creating Pass/Fail 
+df["Result"] = df["Marks"].apply(
+    lambda x: "Pass" if x>=50 else"Fail"
+)
+
+print(df[["Marks","Result"]])
+
+
+#grade feature
+def grade(mark):
+    if pd.isna(mark):
+        return"Missing"
+    elif mark >= 90:
+        return"A"
+    elif mark >=80:
+        return"B"
+    elif mark >=70:
+        return"C"
+    else:
+        return"D"
+
+df["Grade"] = df["Marks"].apply(grade)
+print(df[["Marks", "Grade"]])
+
+
+#Percentage of Marks
+
+df["Percentage"] =(df["Marks"]/100)*100
+print(df[["Marks", "Percentage"]])
+
+
+print(df)
