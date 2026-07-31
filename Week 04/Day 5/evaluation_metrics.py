39040e850dc2048e426dd6e94d5482a1f6fcd919
Muhammad Twaha <twahadayo88@gmail.com>
2026-07-31 11:25:00 +0500

    Week 04 day 5: Caluclating the MAE Mean Absolute Error

---
+++ Week 04/Day 5/evaluation_metrics.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import(
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score
+)
+
+#load dataset
+df= pd.read_csv("Week 04/Day 5/std_score.csv")
+print(df)
+
+#now selecting the feature and target
+
+x = df[["Hours"]]
+y = df[["Marks"]]
+
+#now train test split
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+
+#now creating the linear regression model
+
+model = LinearRegression()
+
+model.fit(x_train, y_train)
+
+y_prediction = model.predict(x_test)
+
+print("Predicted values;")
+print(y_prediction)
+
+#now comparing both actual and predicted values 
+print("\nActual values")
+print(y_test)
+
+print("\n Predicted vlues")
+print(y_prediction)
+
+
+#calculating MAE (Mean Absolute error)
+mae = mean_absolute_error(y_test, y_prediction)
+print("MAE:",mae)
