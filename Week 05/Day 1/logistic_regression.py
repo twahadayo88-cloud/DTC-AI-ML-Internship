ea4ff2aa511fbae003b0128d8edf7326cb86d899
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-03 14:50:28 +0500

    Week 05 day 1: doing another task to predict and find accuracy of students

--- Week 05/Day 1/logistic_regression.py
+++ Week 05/Day 1/logistic_regression.py
@@ -1,4 +1,4 @@
-import pandas as pd
+""""import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -40,4 +40,54 @@ print(y_test.values)
 
 accuracy = accuracy_score(y_test, predictions)
 print("Accuracy:", accuracy_score(y_test, predictions))
-print(f"Model Accuracy: {accuracy * 100:.2f}%")
+print(f"Model Accuracy: {accuracy * 100:.2f}%")"""
+
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+data = pd.read_csv("Week 05/Day 1/student.csv")
+
+print(data.head(5))
+print(data.info())
+print(data.describe())
+print(data.isnull().sum())
+print(data.shape)
+
+
+x = data[["Hours", "Attendance"]]
+y = data["Result"]
+
+# Split the dataset into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+print("Predictions:", y_pred)
+
+#compare predicted values with actual values
+print("Predicted Values:", y_pred)
+print("Actual Values:", y_test.values)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+#probability of passing the exam based on hours studied and attendance
+probability = model.predict_proba(x_test)
+
+print("Prediction Probabilities:")
+print(probability)
+
+
+
