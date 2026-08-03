6a3f5576c4893508fef63a8757a8c165141f5871
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-03 12:44:36 +0500

    Week 05 day 1 : done how we can predict and find accuracy of new data

--- Week 05/Day 1/logistic_regression.py
+++ Week 05/Day 1/logistic_regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # Load the dataset
 data = pd.read_csv("Week 05/Day 1/student.csv")
@@ -28,3 +29,15 @@ model = LogisticRegression()
 
 model.fit(x_train, y_train)
 #print("Model trained successfully.")
+
+predictions = model.predict(x_test)
+print("Predictions:", predictions)
+
+
+print("Predicted Values:", predictions)
+print("Actual Values:",)
+print(y_test.values)
+
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy_score(y_test, predictions))
+print(f"Model Accuracy: {accuracy * 100:.2f}%")
