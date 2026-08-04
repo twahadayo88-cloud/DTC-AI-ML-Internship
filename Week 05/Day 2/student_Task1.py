2b3e9d7d3724ab00a5b54a36ec70577304a3e6b4
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-04 10:45:28 +0500

    Week 05 Day 2: Implementing the KNN model learn how its working and predicting the values plus find the accuracy

---
+++ Week 05/Day 2/student_Task1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+data = pd.read_csv("Week 05/Day 2/student.csv")
+print(data.head())
+
+x = data[["Hours"]]
+y = data["Result"]
+
+# Split the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y, 
+    test_size=0.2, 
+    random_state=42
+)
+
+print(x_train.shape, x_test.shape,
+      y_train.shape, y_test.shape
+)
+
+#creating KNN model
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train, y_train)
+
+#making predictions
+knn_predictions = knn.predict(x_test)
+print("KNN Predictions:", knn_predictions)
+
+#calculating accuracy
+knn_accuracy = accuracy_score(y_test, knn_predictions)
+print("KNN Accuracy:", knn_accuracy)
+
