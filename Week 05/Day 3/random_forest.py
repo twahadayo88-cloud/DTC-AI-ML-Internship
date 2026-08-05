86568d84ff2a8791414b8430a83f08019cad04b2
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-05 11:36:06 +0500

    Week 05 Day 3: Implementing the random forest algorithm and find the the accuracy of model

--- Week 05/Day 3/random_forest.py
+++ Week 05/Day 3/random_forest.py
@@ -12,3 +12,39 @@ print(data.shape)
 print(data.describe())
 print(data.columns)
 print(data.isnull().sum())
+
+x = data.drop("Outcome", axis=1)
+y = data["Outcome"]
+
+print(x.shape)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=(0.2),
+    random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(x_train, y_train)
+y_prediction = model.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_prediction)
+print("Accuracy:", accuracy * 100)
+
+#Feature importance
+
+importance = model.feature_importances_
+#print(importance)
+
+feature_importance = pd.DataFrame({
+    "Feature": x.columns,
+    "Importance": importance
+})
+
+print(feature_importance)
