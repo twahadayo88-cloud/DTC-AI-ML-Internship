7b9497461d8afe9123d3cd386b519947969e654c
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-05 16:14:29 +0500

    Week 03 Final Task

--- Week 03/Week 03 final Tasks/Task_3.py/data.py
+++ Week 03/Week 03 final Tasks/Task_3.py/data.py
@@ -1,14 +1,14 @@
 import pandas as pd
 data=pd.read_csv("Task_3.py/student.csv")
-#print(data) #printing for whole data
-#print(data.head()) #for getting first 5 rows
-#print(data.tail())#for getting last 5 rows
-#print(data.columns) #For column Names
-#print(data.dtypes) #for data type
-#print(data.shape[0]) # for number of rows
-#print(data.shape[1]) #for number of columns
-#print(data["Marks"].describe()) # used for all calculation in one time
-#print(data.min())  #use to calculate minimum value
-#print(data.max())  #use to calculate the maximum value
-#print(data["Marks"].mean()) #use for calculating mean
+print(data) #printing for whole data
+print(data.head()) #for getting first 5 rows
+print(data.tail())#for getting last 5 rows
+print(data.columns) #For column Names
+print(data.dtypes) #for data type
+print(data.shape[0]) # for number of rows
+print(data.shape[1]) #for number of columns
+print(data["Marks"].describe()) # used for all calculation in one time
+print(data.min())  #use to calculate minimum value
+print(data.max())  #use to calculate the maximum value
+print(data["Marks"].mean()) #use for calculating mean
 print(data["Marks"].std()) #use to calculate the standard deviation
