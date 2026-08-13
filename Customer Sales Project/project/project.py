2728f1ff98e669349893c52edde28a4275d6e3c2
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-14 00:48:01 +0500

    Complete Part A dataset understanding and inspection, Part B data cleaning and preprocessing, Part C exploratory data analysis, Part D business analysis visualizations, Part E correlation analysis, Part F feature engineering, Part G high value purchase target, Part H machine learning data preparation, Train Logistic Regression and Decision Tree models, Part J model evaluation and comparison, Part K cross validation, Part L feature importance analysis

--- Customer Sales Project/project/project.py
+++ Customer Sales Project/project/project.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 
 data = pd.read_csv("Customer Sales Project/project/dataset/Customer-Sales-Dataset.csv")
 print("Dataset first 10 rows:",data.head(10))
@@ -19,16 +24,11 @@ print("PaymentMethod:",data["PaymentMethod"].nunique())
 #print("OrderStatus:",data["OrderStatus"].nunique())
 
 #now checking the acutal categorical values 
-
-
 print("\nRegion Transactions:")
 print(data["Region"].value_counts(dropna=False))
-
 print("\nCategory Transactions:")
 print(data["Category"].value_counts())
-
 before_cleaning = data.shape
-
 print("Shape Before Cleaning:", before_cleaning)
 
 data["Region"] = data["Region"].fillna("Unknown")
@@ -38,7 +38,6 @@ data["UnitPrice"] = data["UnitPrice"].fillna(data["UnitPrice"].median())
 print("Missing UnitPrice:", data["UnitPrice"].isnull().sum())
 
 data["DiscountPercent"] = data["DiscountPercent"].fillna(data["DiscountPercent"].median())
-
 print("Missing DiscountPercent:", data["DiscountPercent"].isnull().sum())
 print("\nMissing Values After Handling:")
 print(data.isnull().sum())
@@ -63,22 +62,18 @@ print("\nOrder Status:")
 print(data["OrderStatus"].value_counts())
 
 total_sales = data["Sales"].sum()
-
 print("Total Sales:", total_sales)
 
 total_profit = data["Profit"].sum()
-
 print("Total Profit:", total_profit)
 
 average_sales = data["Sales"].mean()
-
 print("Average Sales:", average_sales)
 
 average_profit = data["Profit"].mean()
-
 print("Average Profit:", average_profit)
-highest_sales = data.loc[data["Sales"].idxmax()]
 
+highest_sales = data.loc[data["Sales"].idxmax()]
 print("\nHighest Sales Transaction:", highest_sales)
 
 lowest_sales = data.loc[data["Sales"].idxmin()]
@@ -89,20 +84,17 @@ print("Highest Sales:", data["Sales"].max())
 print("Lowest Sales:", data["Sales"].min())
 
 total_qty = data["Quantity"].sum()
-
 print("Total Quantity sold:", total_qty)
 
 sales_by_Region = data.groupby("Region")["Sales"].sum()
-
 print("Sales by Region:")
 print(sales_by_Region)
-sales_by_Category = data.groupby("Category")["Sales"].sum()
 
+sales_by_Category = data.groupby("Category")["Sales"].sum()
 print("Sales by Category:")
 print(sales_by_Category)
 
 profit_by_category = data.groupby("Category")["Profit"].sum()
-
 print("Profit by Category:")
 print(profit_by_category)
 
@@ -152,10 +144,8 @@ plt.close()
 
 #--------------
 sales_by_region = data[data["Region"] != "Unknown"].groupby("Region")["Sales"].sum().sort_values(ascending=False) / 10000000
-
 plt.figure(figsize=(10,6))
 sales_by_region.plot(kind='bar', color='pink', edgecolor='black')
-
 plt.title("Sales by Region", fontsize=14, fontweight='bold')
 plt.xlabel("Region", fontsize=12)
 plt.ylabel("Total Sales", fontsize=12)
@@ -176,7 +166,7 @@ plt.xlabel("Category", fontsize=12)
 plt.ylabel("Total Profit", fontsize=12)
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
-plt.savefig("profit_by_category.png")
+#plt.savefig("profit_by_category.png")
 plt.show()
 plt.close()
 
@@ -203,6 +193,269 @@ plt.xlabel("Sales", fontsize=12)
 plt.ylabel("Profit", fontsize=12)
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
-plt.savefig("sales_vs_profit.png")
+#plt.savefig("sales_vs_profit.png")
 plt.show()
 plt.close()
+#-----------------------------------------------------------------------------------
+#part E
+
+correlation_data = data[[
+    "Quantity",
+    "UnitPrice",
+    "DiscountPercent",
+    "Sales",
+    "Cost",
+    "Profit"
+]]
+
+print("\nCorrelation Matrix:")
+print(correlation_data.corr())
+#------------------------------
+corr_matrix = correlation_data.corr()
+corr_pairs = corr_matrix.unstack()
+corr_pairs = corr_pairs[corr_pairs < 1]
+strongest_positive_pair = corr_pairs.idxmax()
+strongest_positive_value = corr_pairs.max()
+
+print("\nStrongest Positive Relationship:")
+print(strongest_positive_pair)
+print("Correlation:", strongest_positive_value)
+
+#-----------------------------
+print("\nCorrelation with Sales:")
+print(correlation_data.corr()["Sales"].sort_values(ascending=False))
+
+#------------------------
+discount_profit_corr = correlation_data["DiscountPercent"].corr(
+    correlation_data["Profit"]
+)
+print("\nDiscountPercent vs Profit Correlation:")
+print(discount_profit_corr)
+#----------------------------------
+print("\nComplete Correlation Matrix:")
+print(corr_matrix)
+#-------------------
+
+# 1. Profit Margin
+data["ProfitMargin"] = (data["Profit"] / data["Sales"]) * 100
+print("\nProfit Margin:")
+print(data[["Sales", "Profit", "ProfitMargin"]].head())
+
+#--------------------
+# 2. Discount Amount
+data["DiscountAmount"] = (data["Sales"] * data["DiscountPercent"]/ (100 - data["DiscountPercent"]))
+print("\nDiscount Amount:")
+print(data[["Sales", "DiscountPercent", "DiscountAmount"]].head())
+
+#--------------------------------
+# 3. Month
+data["Month"] = data["OrderDate"].dt.month
+print("\nMonth:")
+print(data[["OrderDate", "Month"]].head())
+#-------------------------
+# 4. Day of Week
+data["DayOfWeek"] = data["OrderDate"].dt.day_name()
+print("\nDay of Week:")
+print(data[["OrderDate", "DayOfWeek"]].head())
+#--------------------------------------
+# 5. Sales per Item
+data["SalesPerItem"] = data["Sales"] / data["Quantity"]
+print("\nSales per Item:")
+print(data[["Sales", "Quantity", "SalesPerItem"]].head())
+#------------------------------
+# 6. Cost per Item
+data["CostPerItem"] = data["Cost"] / data["Quantity"]
+print("\nCost per Item:")
+print(data[["Cost", "Quantity", "CostPerItem"]].head())
+#-----------------------------------
+
+
+# part G
+sales_median = data["Sales"].median()
+print("\nMedian Sales:", sales_median)
+data["high_value_purchase"] = (data["Sales"] >= sales_median).astype(int)
+print("\nHigh Value Purchase:")
+print(data[["Sales", "high_value_purchase"]].head(10))
+class_counts = data["high_value_purchase"].value_counts().sort_index()
+print("\nClass Counts:")
+print(class_counts)
+class_percentages = (
+    data["high_value_purchase"]
+    .value_counts(normalize=True)
+    .sort_index() * 100
+)
+print("\nClass Percentages:")
+print(class_percentages)
+
+# Part H - Prepareing data for Machine Learning
+
+predictor_features = [
+    "Quantity",
+    "UnitPrice",
+    "DiscountPercent",
+    "Cost",
+    "Profit",
+    "Region",
+    "Category",
+    "PaymentMethod",
+    "OrderStatus"
+]
+target = "high_value_purchase"
+X = data[predictor_features]
+y = data[target]
+print("\nPredictor Features:")
+print(X.columns)
+print("\nTarget:")
+print(y.name)
+#----------------------
+X = pd.get_dummies(
+    X,
+    columns=[
+        "Region",
+        "Category",
+        "PaymentMethod",
+        "OrderStatus"
+    ],
+    drop_first=True
+)
+print("\nEncoded Features:")
+print(X.head())
+print("\nX Shape:", X.shape)
+print("y Shape:", y.shape)
+print("\nNumber of Model Features:", X.shape[1])
+
+#train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+print("\nTraining Data Shape:")
+print(X_train.shape)
+print("\nTesting Data Shape:")
+print(X_test.shape)
+print("\nTraining Labels Shape:")
+print(y_train.shape)
+print("\nTesting Labels Shape:")
+print(y_test.shape)
+
+#---------------
+#part I
+# Logistic Regression
+logistic_model = LogisticRegression(max_iter=1100, random_state=42)
+logistic_model.fit(X_train, y_train)
+print("\nLogistic Regression model trained successfully.")
+logistic_predictions = logistic_model.predict(X_test)
+print("\nLogistic Regression Predictions:")
+print(logistic_predictions[:11])
+
+# Decision Tree
+
+decision_tree_model = DecisionTreeClassifier(random_state=42)
+decision_tree_model.fit(X_train, y_train)
+print("Decision Tree model trained successfully.")
+decision_tree_predictions = decision_tree_model.predict(X_test)
+print("\nDecision Tree Predictions:")
+print(decision_tree_predictions[:11])
+
+#Accuracy Scores
+from sklearn.metrics import accuracy_score
+logistic_accuracy = accuracy_score(y_test, logistic_predictions)
+decision_tree_accuracy = accuracy_score(y_test, decision_tree_predictions)
+print("\nLogistic Regression Accuracy:", logistic_accuracy)
+print("Decision Tree Accuracy:", decision_tree_accuracy)
+
+#feature importance for decision tree
+feature_importance = decision_tree_model.feature_importances_
+print("\nFeature Importance (Decision Tree):")
+for feature, importance in zip(X.columns, feature_importance):
+    print(f"{feature}: {importance:.4f}")
+
+
+#confusion matrix for decision tree
+from sklearn.metrics import confusion_matrix
+decision_tree_cm = confusion_matrix(y_test, decision_tree_predictions)
+print("\nConfusion Matrix (Decision Tree):")
+print(decision_tree_cm)
+
+#confusion matrix for logistic regression
+logistic_cm = confusion_matrix(y_test, logistic_predictions)
+print("\nConfusion Matrix (Logistic Regression):")
+print(logistic_cm)
+logistic_coefficients = pd.Series(
+    logistic_model.coef_[0],
+    index=X_train.columns
+).sort_values(ascending=False)
+print("\nLogistic Regression Coefficients:")
+print(logistic_coefficients)
+
+#--------------------------
+#Part J
+#Calculate accuracy, Precision, Recall, and F1-Score for Decision Tree
+
+from sklearn.metrics import classification_report
+decision_tree_classification_report = classification_report(y_test, decision_tree_predictions)
+print("\nClassification Report (Decision Tree):")
+print(decision_tree_classification_report)
+
+#Calculate accuracy, Precision, Recall, and F1-Score for Logistic Regression
+logistic_classification_report = classification_report(y_test, logistic_predictions)
+print("\nClassification Report (Logistic Regression):")
+print(logistic_classification_report)
+#confusion matrix for decision tree
+print("\nConfusion Matrix (Decision Tree):")
+print(decision_tree_cm)
+#confusion matrix for logistic regression
+print("\nConfusion Matrix (Logistic Regression):")
+print(logistic_cm)
+
+#part K
+#cross validation for decision tree
+cv_scores = cross_val_score(
+    decision_tree_model,
+    X_train,
+    y_train,
+    cv=5,
+    scoring="accuracy"
+)
+print("\n5-Fold Cross Validation Scores:")
+print(cv_scores)
+average_cv_score = cv_scores.mean()
+print("\nAverage Validation Score:")
+print(average_cv_score)
+decision_tree_test_predictions = decision_tree_model.predict(X_test)
+test_accuracy = accuracy_score(
+    y_test,
+    decision_tree_test_predictions
+)
+print("\nTest Accuracy:")
+print(test_accuracy)
+
+#part L
+feature_importance = pd.Series(
+    decision_tree_model.feature_importances_,
+    index=X_train.columns
+)
+print("\nFeature Importance:")
+print(feature_importance.sort_values(ascending=False))
+
+#top 5 features
+top_5_features = (
+    feature_importance
+    .sort_values(ascending=False)
+    .head(5)
+)
+print("\nTop 5 Most Important Features:")
+print(top_5_features)
+
+#creating bar chart for top 5 features
+plt.figure(figsize=(10,6))
+top_5_features.plot(kind="bar")
+plt.title("Top 5 Most Important Features (Decision Tree)")
+plt.xlabel("Features")
+plt.ylabel("Importance Score")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig("top_5_features_decision_tree.png")
+plt.show()
