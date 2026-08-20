9e7c27c0e1bf8c35e38134d1d65216a730c09dd2
Muhammad Twaha <twahadayo88@gmail.com>
2026-08-20 15:38:37 +0500

    Week 07 Day 1: Implement the sentence and word tokenize part to understand the data.

--- Week 07/Day 1/task.py
+++ Week 07/Day 1/task.py
@@ -1,72 +1,27 @@
-import nltk
 import pandas as pd
-from nltk.probability import FreqDist
-from nltk.corpus import stopwords
-import string
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
 
-print("NLTK is ready!")
+#nltk tokenizer download karny ky lia
+nltk.download('punkt')
+nltk.download('punkt_tab')
 
-text = """
-Artificial Intelligence is changing the world.
-Machine learning helps businesses make better decisions.
-Natural Language Processing allows computers to understand human language.
-Python is widely used for data science and machine learning.
-AI systems can analyze large amounts of text quickly.
-"""
+data = pd.read_csv("Week 07/Day 1/customer.csv")
 
-print("Original Text:")
-print(text)
+print(data.head())
+print("Dataset Shape:", data.shape)
+print("Column Name:", data.columns)
+print("Missing Values:", data.isnull().sum())
 
-"""sentences = nltk.sent_tokenize(text)
-print("\nTokenized Sentences:")
-print(sentences)"""
+#Word Tokenize
+sample_text = data["ReviewText"].iloc[0]
+print("Sample Text:", sample_text)
 
-words = nltk.word_tokenize(text)
-print("\nTokenized Words:")
+#Sentence Tokenize
+sentences = sent_tokenize(sample_text)
+print("Sentence Tokens:", sentences)
+
+#Word Tokenize
+words = word_tokenize(sample_text)
+print("Work Tokens:")
 print(words)
-
-print("\nTotal Tokens:", len(words))
-
-frequency = FreqDist(words)
-print("\nWord Frequency:")
-print(frequency)
-
-print("\nTop 10 Most Common Words:")
-
-for word, count in frequency.most_common(10):
-    print(word, ":", count)
-
-stop_words = set(stopwords.words("english"))
-filtered_words = [word for word in words if word.lower() not in stop_words]
-print("\nAfter stopwords removal:")
-print(filtered_words)
-
-clean_words = [
-    word for word in filtered_words
-    if word not in string.punctuation
-]
-
-print("\nWords After Punctuation Removal:")
-print(clean_words)
-
-stemmer = PorterStemmer()
-
-stemmed_words = [
-    stemmer.stem(word)
-    for word in clean_words
-]
-
-print("\nStemmed Words:")
-print(stemmed_words)
-
-lemmatizer = WordNetLemmatizer()
-
-lemmatized_words = [
-    lemmatizer.lemmatize(word)
-    for word in clean_words
-]
-
-print("\nLemmatized Words:")
-print(lemmatized_words)
